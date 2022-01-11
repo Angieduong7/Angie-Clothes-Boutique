85028eb1cadf0dc57195a4267ee26068ba31346f
Angieduong7 <th.nguyenduong94@gmail.com>
2022-01-12 09:56:18 +1000

    finish sign in and sign up functionality

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../fire-base/firebase.utils';
+import { auth, signInWithGoogle } from '../../fire-base/firebase.utils';
 
 class SignIn extends React.Component {
   constructor() {
@@ -15,9 +15,16 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault();
-    this.setState({ email: '', password: '' });
+
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = (e) => {
