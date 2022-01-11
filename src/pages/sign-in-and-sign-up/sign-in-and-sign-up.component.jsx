c14080ace0cec0e31de8a5fbba75587df787469a
Angieduong7 <th.nguyenduong94@gmail.com>
2022-01-12 09:11:21 +1000

    implemented firebase utils to store authenticated users into firebase database

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './sign-in-and-sign-up.style.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInAndSignUpPage = () => {
-  return <SignIn className='sign-in-and-sign-up' />;
+  return (
+    <div className='sign-in-and-sign-up'>
+      <SignIn />;
+      <SignUp />
+    </div>
+  );
 };
 
 export default SignInAndSignUpPage;
