c5e88907a332955cd6d67839da6c4a7a96302915
Angieduong7 <th.nguyenduong94@gmail.com>
2022-01-14 14:06:24 +1000

    added card Icon and implement cart dropdown

---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,7 @@
+import cartTypes from './cart.types';
+
+const toggleCartHidden = {
+  type: cartTypes.toggle_cart_hidden,
+};
+
+export default toggleCartHidden;
---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,5 @@
+const cartTypes = {
+  toggle_cart_hidden: 'TOGGLE_CART_HIDDEN',
+};
+
+export default cartTypes;
